3c8009ab387b070bd21e3a2994b92312bcea8133
Sailor Winkelman and Tyler Bussanich <tyler@>
2018-01-17 11:50:21 -0800

    Create repository and environment using Angular CLI. Add new hard-coded keg objects to html template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  currentKeg: string = 'Kegs';
+  kegs: Keg [] = [
+    new Keg('Pliny the Younger', 'Russian River', '$6.50', '10.25%', '58'),
+    new Keg('LSD', 'Lompoc', '$5.50', '6.9%', '90')
+  ];
+  testKeg(){
+    this.kegs.push(new Keg ('Sailors Beer', 'Brewery  Co.', '$2.00', '6%', '45'));
+  }
+  addKeg(name, brewery, price, ABV, IBU){
+    this.kegs.push(new Keg(name, brewery, price, ABV, IBU));
+    console.log(this.addKeg(name, brewery, price, ABV, IBU));
+  }
+}
+
+export class Keg {
+  constructor(public name: string, public brewery: string, public price: string, public ABV: string, public IBU: string) { }
 }
